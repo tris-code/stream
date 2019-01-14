ae1804c2298f9e00c0f6803ca7710f57161051f2
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 22:32:38 +0000

    [Cleanup] Delete copyright notice

--- Sources/Stream/Library/BufferedInputStream+StreamReader.swift
+++ Sources/Stream/Library/BufferedInputStream+StreamReader.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 extension BufferedInputStream: StreamReader {
     public func cache(count: Int) throws -> Bool {
         if count > buffered {
